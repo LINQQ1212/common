135673dbc400d59c2d5e1cc566f78c6b9c0366af
LINQQ1212 <1142798229@qq.com>
2022-12-22 16:29:35 +0800

    v1 -ky

--- models/pro.go
+++ models/pro.go
@@ -4,24 +4,9 @@ func (x *Product) Copy(p *Product) {
 	x.ID = p.ID
 	x.CateId = p.CateId
 	x.DomainID = p.DomainID
-	x.Pid = p.Pid
-	x.Model = p.Model
-	x.Image = p.Image
-	x.Images = p.Images
-	x.Price = p.Price
-	x.Specials = p.Specials
 	x.Name = p.Name
-	x.Description = p.Description
-	x.MTitle = p.MTitle
-	x.MKeywords = p.MKeywords
-	x.MDescription = p.MDescription
+	x.Image = p.Image
 	x.Link = p.Link
 	x.Keywords = p.Keywords
-	x.CPath = p.CPath
-	x.GoogleImgs = p.GoogleImgs
-	x.YahooDesc = p.YahooDesc
-	x.Categories = p.Categories
 	x.Brand = p.Brand
-	x.BingDesc = p.BingDesc
-	x.Youtube = p.Youtube
 }
