1e7802c575daca7e33aab2ad97ac6d13b31d2228
LINQQ1212 <1142798229@qq.com>
2022-12-24 13:46:55 +0800

    v1 end

--- admin/router/init_router.go
+++ admin/router/init_router.go
@@ -22,6 +22,7 @@ func InitRouter(r *gin.Engine) {
 		a.GET("/new-version", apis.Admin)
 		a.GET("/new-domain", apis.Admin)
 		a.GET("/login", apis.Admin)
+
 	}
 	r.NoRoute(func(c *gin.Context) {
 		c.Writer.WriteString("404")
