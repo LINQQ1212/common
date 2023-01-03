2773c5adb2c88e8e420cbe74b4a7e005f9292928
LINQQ1212 <1142798229@qq.com>
2023-01-03 17:55:51 +0800

    v1 end

--- admin/service/new_version.go
+++ admin/service/new_version.go
@@ -4,16 +4,19 @@ import (
 	"github.com/LINQQ1212/common/create_db"
 	"github.com/LINQQ1212/common/global"
 	"github.com/LINQQ1212/common/models"
+	"go.uber.org/zap"
 )
 
 func NewVersion(req models.NewVersionReq) error {
 	cdb := create_db.New(req)
 	err := cdb.Start()
 	if err != nil {
+		global.LOG.Error("Start", zap.Error(err))
 		return err
 	}
 	v, err := models.NewVersion(global.VersionDir, cdb.GetDomain())
 	if err != nil {
+		global.LOG.Error("NewVersion", zap.Error(err))
 		return err
 	}
 	global.Versions.Set(cdb.GetDomain(), v)
